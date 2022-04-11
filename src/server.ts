f1c89d3d2e2eb13f689ce80250165747a6819ad3
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-04-11 01:55:07 -0300

    chore: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createCourse } from './routes';
 
 const app = express();
 
-app.get("/", createCourse);
+app.get("/", (request, response) => {
+  return response.json({ message: "Server is running..." });
+})
 
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running..."));
