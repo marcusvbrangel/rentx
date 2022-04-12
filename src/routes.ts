c59e8e763bb270c82f466241d816fc7af3103436
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-04-11 22:49:48 -0300

    chore: config debuguer

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,9 @@ import { Request, Response } from "express";
 
 import CreateCourseService from "./CreateCourseService";
 
-export const createCourse = (
-  request: Request,
-  response: Response
-): Response => {
+// https://github.com/prettier/eslint-plugin-prettier#options
+// eslint-disable-next-line prettier/prettier
+export const createCourse = (request: Request, response: Response): Response => {
   CreateCourseService.execute({
     name: "Nodejs",
     educator: "Nemarvin",
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Server is running..." });
-});
+app.use(express.json());
 
 app.listen(3333, () => console.log("Server is running..."));
